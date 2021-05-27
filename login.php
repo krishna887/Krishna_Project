f16165924b436024c1e525738ed79e3241ca993e
krishna chaulagain <66787324+krishna887@users.noreply.github.com>
2021-05-27 22:09:04 +0545

    Add files via upload

---
+++ login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>dashboard</title>
+</head>
+<body>
+	<button>Back</button>
+	<button>Logout</button>
+<h1>ONLINE VOTING SYSTEM</h1>
+<hr>
+<div id="profile"></div>
+<div id="group"></div>
+</body>
+</html>
